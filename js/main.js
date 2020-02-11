d7016d0fea2e155fb4293cfb403b177288a90ae2
MilcahHalili <halilicenteno@gmail.com>
2020-02-10 22:45:58 -0800

    start creating constants and app's state (variables)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 /*---- constants ----*/
+const MAX_WRONG_GUESSES = 6;
+const WORDS = [
+  'VARIABLE', 'FUNCTION', 'OBJECT',
+  'ENCAPSULATION', 'ARRAY', 'MVC',
+  'REACT', 'CLASS', 'CALLBACK'
+];
+
 /*---- app's state (variables) ----*/
+let usedLetters, wrongGuesses, secretWord = [];
+
 /*---- cached element references ----*/
+
+
 /*---- event listeners ----*/
+
+
 /*---- functions ----*/
+function init() {
+
+}
+
+function render() {
+
+}
